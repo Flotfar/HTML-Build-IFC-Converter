d2831451d11e2d69d7d0360ca4fa7f7c4e3f2dda
Flotfar <113911826+Flotfar@users.noreply.github.com>
2022-10-15 22:57:28 +0200

    Update html-build.js

--- output/js/html-build.js
+++ output/js/html-build.js
@@ -1,11 +1,11 @@
 /* written by Tim McGinley 2022 */
+/* Editted by Joakim Mørk, Valdemar Rasmussen, Jonas Munch, Oscar Hansen 2022 */
 
-// ok in here we need to include a lot of stuff.
-// we need a menu... where would this fit?
-// we need to start (over)loading stuff into the DOM.
-// we need to split the screen into section and plan and KPIs and info - where should this go?
-
-
+// Aditions made to 'html-build.js':
+// - Calculate num. beams and columns for structural building entities, adding them to properties box (17-38)
+// - 
+// -
+// -
 
 function main() {
 	
@@ -13,13 +13,29 @@ function main() {
 	const floors = document.getElementsByTagName("floor-");
 	let num_floors = floors.length;
 	console.log(num_floors);
+
+	// calculate the beams
+	const beams = document.getElementsByTagName("beam-");
+	let num_beams = beams.length;
+	console.log(num_beams);
+
+	// calculate the columns
+	const columns = document.getElementsByTagName("column-");
+	let num_columns = columns.length;
+	console.log(num_columns);
 	
+
 	// add data to the properties box
-	$('props-').prepend('number of floors is '+num_floors);
+	$('props-').prepend('number of beams is '+num_beams+'');
+	$('props-').prepend('number of columns is '+num_columns+'<br>');
+	$('props-').prepend('STRUCTURAL PROPERTIES:'+'<br>');
+	$('props-').prepend(''+'<br>');
+	$('props-').prepend('number of floors is '+num_floors+'<br>');
 	$('props-').prepend('site elevation is '+$('site-').attr('elev')+'<br>');
+	$('props-').prepend('BUILDING PROPERTIES:'+'<br>');
 	
 	// load the plan so we can edit it
-	plan('happy');
+	plan('Group 14 - HTML.Build <br> <br> Click a floor to see the plan');
 	
 	// The .each() method is unnecessary here:
 	$( 'floor-' ).each(function() {
@@ -27,7 +43,8 @@ function main() {
 		$( this ).on("click", function(){
 			//$('plan-').css("background-color","black");
 			
-			changePlan($(this).attr('name')+':'+$(this).attr('level'));
+			changePlan($(this).attr('name') + ':' + $(this).attr('level') + '<br><br> Beams:');
+			
 			//$( this ).innerHTML
 		});
 	});
